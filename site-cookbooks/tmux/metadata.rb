7eeeb53ea3f6d205b1eaa9093c63454b92be0620
Paul Stadig <paul@stadig.name>
2010-10-20 15:08:38 -0400

    Add devilspie and tmux cookbooks with samson JSON file.

---
+++ site-cookbooks/tmux/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Paul Stadig"
+maintainer_email "paul@stadig.name"
+license          "Apache 2.0"
+description      "Installs/Configures tmux"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
