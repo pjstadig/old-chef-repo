dd9c2c58afe922ac1383fc3b579593e5dcdf7c86
Paul Stadig <paul@stadig.name>
2010-10-20 10:06:47 -0400

    Added bootstrap script

---
+++ solo.rb
@@ -0,0 +1,5 @@
+node_name       "client"
+file_cache_path "/tmp/vagrant-chef"
+cookbook_path   ["/home/paul/src/chef-repo/cookbooks","/home/paul/src/chef-repo/site-cookbooks"]
+role_path       "/home/paul/src/chef-repo/roles"
+log_level       :info
