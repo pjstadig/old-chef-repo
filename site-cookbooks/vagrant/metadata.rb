6463df7448965617cb1e7fe70f12f1a69f928954
Paul Stadig <paul@stadig.name>
2010-10-20 15:53:20 -0400

    Add virtualbox and vagrant recipies.

---
+++ site-cookbooks/vagrant/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Paul Stadig"
+maintainer_email "paul@stadig.name"
+license          "Apache 2.0"
+description      "Installs/Configures vagrant"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
+dependencies     "virtualbox"
