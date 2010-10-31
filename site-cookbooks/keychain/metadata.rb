627626f4db6b8409b63210c32997c448326e0029
Paul Stadig <paul@stadig.name>
2010-10-31 19:23:26 -0400

    Add keychain recipe.

---
+++ site-cookbooks/keychain/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Paul Stadig"
+maintainer_email "paul@stadig.name"
+license          "Apache 2.0"
+description      "Installs/Configures keychain"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
