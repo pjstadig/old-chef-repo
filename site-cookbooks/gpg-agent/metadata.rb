487781742aa0bebe249de10fd565fd7c55cdcaaf
Paul Stadig <paul@stadig.name>
2010-11-01 10:24:44 -0400

    Add gpg-agent

---
+++ site-cookbooks/gpg-agent/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Paul Stadig"
+maintainer_email "YOUR_EMAIL"
+license          "Apache 2.0"
+description      "Installs/Configures gpg-agent"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
